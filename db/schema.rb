7c19c295fabf68e4132b78c7ffa5d6cf68f980d2
Francesca Santoriello <f.santoriello@hotmail.com>
2023-08-15 09:30:33 +0200

    add rating to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_071412) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_072328) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating", default: 0
   end
+
 end
