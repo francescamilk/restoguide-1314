f4e1bc8aa8e93eba4bf21fc170b57c15490e911b
Francesca Santoriello <f.santoriello@hotmail.com>
2023-08-16 09:20:28 +0200

    import bootstrap + simple form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,4 +9,19 @@ class RestaurantsController < ApplicationController
         id = params[:id]
         @restaurant = Restaurant.find(id)
     end
+
+    def new
+        @restaurant = Restaurant.new
+    end
+
+    def create
+        @restaurant = Restaurant.new(restaurant_params)
+        redirect_to restaurant_path if @restaurant.save
+    end
+
+    private
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name, :address, :rating)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  post 'restaurants', to: 'restaurants#create'
+  
   # Read (all)
   # GET localhost:3000/restaurants
   get 'restaurants', to: 'restaurants#index'
@@ -7,4 +10,5 @@ Rails.application.routes.draw do
   # GET localhost:3000/restaurants/1
   # GET localhost:3000/restaurants/4
   get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
 end
